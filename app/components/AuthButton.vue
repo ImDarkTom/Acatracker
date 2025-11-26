ab902c82abb115c3601625dc4a5e60002a0012be
ImDarkTom <darktomg21@gmail.com>
2025-11-26 12:57:54 +0000

    feat: transitions

--- app/components/AuthButton.vue
+++ app/components/AuthButton.vue
@@ -15,31 +15,33 @@ const authStore = useAuthStore();
             </AppBtnPrimary>
         </DropdownMenuTrigger>
         <DropdownMenuPortal>
-            <DropdownMenuContent 
-                :align="'start'"
-                :align-offset="16"
-                class="w-52 shadow-sm bg-elevated rounded-md overflow-hidden mx-2 z-100">
-                <CustomDropdownItem 
-                    value="Dashboard" 
-                    icon="material-symbols:dashboard-outline-rounded" 
-                    :select-action="() => navigateTo('/dashboard')" />
-                <CustomDropdownItem 
-                    value="Add Assesment" 
-                    icon="material-symbols:add-notes-outline-rounded" 
-                    :select-action="() => navigateTo('/dashboard')" />
-                <DropdownMenuSeparator class="h-px w-full bg-brand-50" />
-                <RouterLink to="/dashboard/subscribe">
+            <Transition name="dropdown">
+                <DropdownMenuContent 
+                    :align="'start'"
+                    :align-offset="16"
+                    class="w-52 shadow-sm bg-elevated rounded-md overflow-hidden mx-2 z-100">
                     <CustomDropdownItem 
-                        value="Subscribe" 
-                        icon="material-symbols:calendar-add-on-outline-rounded" />
-                </RouterLink>
-                <DropdownMenuSeparator class="h-px w-full bg-brand-50" />
-                <CustomDropdownItem 
-                    value="Sign Out"
-                    icon="material-symbols:logout-rounded" 
-                    :select-action="() => navigateTo('/sign-out')" />
-                <DropdownMenuArrow class="fill-elevated" />
-            </DropdownMenuContent>
+                        value="Dashboard" 
+                        icon="material-symbols:dashboard-outline-rounded" 
+                        :select-action="() => navigateTo('/dashboard')" />
+                    <CustomDropdownItem 
+                        value="Add Assesment" 
+                        icon="material-symbols:add-notes-outline-rounded" 
+                        :select-action="() => navigateTo('/dashboard')" />
+                    <DropdownMenuSeparator class="h-px w-full bg-brand-50" />
+                    <RouterLink to="/dashboard/subscribe">
+                        <CustomDropdownItem 
+                            value="Subscribe" 
+                            icon="material-symbols:calendar-add-on-outline-rounded" />
+                    </RouterLink>
+                    <DropdownMenuSeparator class="h-px w-full bg-brand-50" />
+                    <CustomDropdownItem 
+                        value="Sign Out"
+                        icon="material-symbols:logout-rounded" 
+                        :select-action="() => navigateTo('/sign-out')" />
+                    <DropdownMenuArrow class="fill-elevated" />
+                </DropdownMenuContent>
+            </Transition>
         </DropdownMenuPortal>
     </DropdownMenuRoot>
     <AppBtnPrimary 
--- app/components/custom/Dialog.vue
+++ app/components/custom/Dialog.vue
@@ -36,32 +36,36 @@ function handleExitClick(e: Event, exitFn: () => void) {
             <slot name="button" />
         </DialogTrigger>
         <DialogPortal>
-            <DialogOverlay class="bg-black/35 fixed inset-0 z-30" />
-            <DialogContent
-                @escape-key-down="handleExitClick($event, dialogClose)"
-                @pointer-down-outside="handleExitClick($event, dialogClose)"
-                class="p-4 bg-base fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-h-[85vh] h- w-md max-w-full rounded-lg shadow-md shadow-black z-100">
-                <DialogTitle class="text-lg font-semibold text-brand-100">
-                    <slot name="title" />
-                </DialogTitle>
-                <DialogDescription class="mt-2 mb-1">
-                    <slot name="description" />
-                </DialogDescription>
-                <div v-if="submitError" class="bg-errorbg p-2 rounded-sm">
-                    {{ submitError }}
-                </div>
-
-                <slot name="form" />
-
-                <button
-                    class="absolute top-4 right-4"
-                    aria-label="Close"
-                    @click="handleExitClick($event, dialogClose)">
-                    <div class="inline-flex cursor-pointer rounded-full p-2 hover:bg-elevated active:shadow-brand-700 shadow-sm">
-                        <Icon name="material-symbols:close-rounded" size="20" />
+            <Transition name="fade">
+                <DialogOverlay class="bg-black/65 backdrop-blur-xs fixed inset-0 z-105" />
+            </Transition>
+            <Transition name="scale">
+                <DialogContent
+                    @escape-key-down="handleExitClick($event, dialogClose)"
+                    @pointer-down-outside="handleExitClick($event, dialogClose)"
+                    class="p-4 bg-base fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-h-[85vh] h- w-md max-w-full rounded-lg shadow-md shadow-black/50 z-110">
+                    <DialogTitle class="text-lg font-semibold text-brand-100">
+                        <slot name="title" />
+                    </DialogTitle>
+                    <DialogDescription class="mt-2 mb-1">
+                        <slot name="description" />
+                    </DialogDescription>
+                    <div v-if="submitError" class="bg-errorbg p-2 rounded-sm">
+                        {{ submitError }}
                     </div>
-                </button>
-            </DialogContent>
+
+                    <slot name="form" />
+
+                    <button
+                        class="absolute top-4 right-4"
+                        aria-label="Close"
+                        @click="handleExitClick($event, dialogClose)">
+                        <div class="inline-flex cursor-pointer rounded-full p-2 hover:bg-elevated active:shadow-brand-700 shadow-sm">
+                            <Icon name="material-symbols:close-rounded" size="20" />
+                        </div>
+                    </button>
+                </DialogContent>
+            </Transition>
         </DialogPortal>
     </DialogRoot>
 </template>
