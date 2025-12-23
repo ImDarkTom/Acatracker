92fefa11410f7220260529fba3d96aa2724e71f9
ImDarkTom <darktomg21@gmail.com>
2025-12-23 03:37:17 +0000

    feat: animate calendar entry popover

--- app/components/dashboard/CalendarEntry.vue
+++ app/components/dashboard/CalendarEntry.vue
@@ -33,31 +33,35 @@ const infoOpened = ref(false);
             </div>
         </PopoverTrigger>
         <PopoverPortal>
-            <PopoverContent 
-                side="left"
-                class="card bg-bg-active flex flex-col gap-2 shadow-md shadow-shadow-base p-4 min-w-60 max-w-md">
-                <span :class="{
-                    'text-event-due': eventProp.type === 'due',
-                    'text-event-released': eventProp.type === 'released',
-                    'text-event-task': eventProp.type === 'task',
-                }">{{ eventTypeMap[eventProp.type] }}</span>
-                <span class="font-medium text-text text-lg">{{ eventProp.label }}</span>
-                <label class="flex flex-row gap-2">
-                    <span>Completed?</span>
-                    <input type="checkbox" :value="eventProp.completed">
-                </label>
-                <RouterLink :to="eventProp.link">
-                    <ButtonPrimary>
-                    View Details
-                    </ButtonPrimary>
-                </RouterLink>
-                <PopoverArrow class="fill-highlight" />
-                <PopoverClose class="absolute top-2 right-2">
-                    <div class="inline-flex cursor-pointer rounded-full p-2 hover:bg-highlight active:bg-bg-focus">
-                        <Icon name="material-symbols:close-rounded" size="20" />
-                    </div>
-                </PopoverClose>
-            </PopoverContent>
+            <Transition name="fade">
+                <PopoverContent 
+                    :avoid-collisions="true"
+                    :collision-padding="8"
+                    :side-offset="8"
+                    class="card bg-bg-active flex flex-col gap-2 shadow-md shadow-shadow-base p-4 min-w-60 max-w-md">
+                    <span :class="{
+                        'text-event-due': eventProp.type === 'due',
+                        'text-event-released': eventProp.type === 'released',
+                        'text-event-task': eventProp.type === 'task',
+                    }">{{ eventTypeMap[eventProp.type] }}</span>
+                    <span class="font-medium text-text text-lg">{{ eventProp.label }}</span>
+                    <label class="flex flex-row gap-2">
+                        <span>Completed?</span>
+                        <input type="checkbox" :value="eventProp.completed">
+                    </label>
+                    <RouterLink :to="eventProp.link">
+                        <ButtonPrimary>
+                        View Details
+                        </ButtonPrimary>
+                    </RouterLink>
+                    <PopoverArrow class="fill-highlight" />
+                    <PopoverClose class="absolute top-2 right-2">
+                        <div class="inline-flex cursor-pointer rounded-full p-2 hover:bg-highlight active:bg-bg-focus">
+                            <Icon name="material-symbols:close-rounded" size="20" />
+                        </div>
+                    </PopoverClose>
+                </PopoverContent>
+            </Transition>
         </PopoverPortal>
     </PopoverRoot>
 </template>
