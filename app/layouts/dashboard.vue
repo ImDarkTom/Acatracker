13ff7f7224329fc7e93b53c4d65cbb0cfae43f39
ImDarkTom <darktomg21@gmail.com>
2026-02-08 01:45:48 +0000

    fix: scrolling on dashboard

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="min-h-screen flex flex-col">
         <AppNavBar />
-        <div class="w-full min-h-[calc(100dvh-4.5rem)] flex flex-col md:flex-row gap-4 p-4">
+        <div class="w-full h-[calc(100dvh-4.5rem)] flex flex-col md:flex-row gap-4 p-4">
             <div class="w-full md:w-1/3 min-h-[calc(50dvh-4rem)] md:min-h-full">
                 <DashboardAssessmentList />
             </div>
