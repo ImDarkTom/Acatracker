6a921b8e75c53345b46f70c0d6a109263cc3aa22
ImDarkTom <darktomg21@gmail.com>
2026-02-09 11:53:51 +0000

    Revert "chore: update deps"
    This reverts commit c433b39a94f6eb7bec424ba99d95b07338ec73ea.

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { auth, UserWithId } from "~~/lib/auth";
 
-const UNAUTHED_ONLY_ROUTES = ['/', '/sign-in', '/sign-up'];
-const UNVERIFIED_EMAIL_ONLY_ROUTES = ['/verify-email'];
+const UNAUTHED_ONLY_ROUTES = ['/', '/auth/sign-in', '/auth/sign-up'];
+const UNVERIFIED_EMAIL_ONLY_ROUTES = ['/auth/verify-email'];
 
 export default defineEventHandler(async (event) => {
     const session = await auth.api.getSession({
@@ -19,7 +19,7 @@ export default defineEventHandler(async (event) => {
     if (user) {
         // Redirect unverified users to email verification page
         if (!user.emailVerified && !UNVERIFIED_EMAIL_ONLY_ROUTES.includes(event.path)) {
-            return sendRedirect(event, '/verify-email', 302);
+            return sendRedirect(event, '/auth/verify-email', 302);
         } 
         
         // Redirect authorized users away from auth pages
