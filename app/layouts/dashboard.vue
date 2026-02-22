085fb2de0653ef33807169f85f1f4dd041ef6790
ImDarkTom <darktomg21@gmail.com>
2026-02-22 20:33:06 +0000

    feat: mobile navbar

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -14,7 +14,7 @@ await callOnce('userPreferences', async () => {
     <div class="h-screen flex flex-col">
         <DashboardNavBar />
         <div class="grow min-h-0 flex flex-row">
-            <div class="flex flex-col gap-2 p-2">
+            <nav class="hidden sm:flex flex-col gap-2 p-2">
                 <DashboardSidenavButton
                     label="Home"
                     icon="lucide:home"
@@ -29,15 +29,34 @@ await callOnce('userPreferences', async () => {
                     label="Timeline"
                     icon="lucide:route"
                     to="/dashboard/timeline" />
+            </nav>
+            <div class="sm:hidden w-full fixed bottom-0 pb-2 flex justify-center">
+                <nav class="w-fit flex flex-row bg-bg-app p-2 px-6 gap-4 rounded-full">
+                    <DashboardSidenavButton
+                        label="Home"
+                        icon="lucide:home"
+                        to="/dashboard" />
+                    
+                    <DashboardSidenavButton
+                        label="Calendar"
+                        icon="lucide:calendar"
+                        to="/dashboard/calendar" />
+                        
+                    <DashboardSidenavButton
+                        label="Timeline"
+                        icon="lucide:route"
+                        to="/dashboard/timeline" />
+                </nav>
             </div>
-            <div class="grow flex flex-col md:flex-row bg-bg-base border-t border-l border-highlight rounded-tl-md">
+            <div class="grow flex flex-col md:flex-row bg-bg-base border-t sm:border-l border-highlight sm:rounded-tl-md">
                 <div class="w-full min-h-[calc(50dvh-4rem)] border-highlight border-b-[0.5px]
                     md:w-1/3 md:min-h-full md:border-r-[0.5px]">
                     <DashboardAssessmentList />
                 </div>
                 <div class="flex size-full p-2
                     md:w-2/3 min-h-[calc(50dvh-4rem)] 
-                    md:min-h-full">
+                    md:min-h-full
+                    pb-20 sm:pb-0">
                     <slot />
                 </div>
             </div>
